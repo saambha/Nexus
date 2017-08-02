e798d66d469da266655c3e0ba6b07de156d06b6e
anoop <anoop.sharma@emirates.com>
2017-08-02 22:56:02 +0400

    Initial Commit

---
+++ Nexus/Nexus/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Nexus
+//
+//  Created by anoop on 8/2/17.
+//  Copyright © 2017 anoop. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
